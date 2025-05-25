c368b8c710c5b739553e72815e6ad47f6014cde1
burkex12 <burkem1217@gmail.com>
2025-05-24 23:54:56 -0400

    Add files via upload

---
+++ app 2.py
@@ -0,0 +1,7 @@
+# Dynasty Football Tool MVP
+
+import streamlit as st
+
+st.set_page_config(page_title='Dynasty Football Tool')
+st.title('Dynasty Football Management MVP')
+st.write('Welcome to the Dynasty Football Tool!')
